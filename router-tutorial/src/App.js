145af650b113cef419da624aca83df2117f7898c
borimong <kimhyunsu11@naver.com>
2022-08-13 05:02:20 +0900

    router-tutorial finished

---
+++ router-tutorial/src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
+import About from "./pages/About";
+import Article from "./pages/Article";
+import Articles from "./pages/Articles";
+import Home from "./pages/Home";
+import React from "react";
+import Profile from "./pages/Profile";
+import NotFound from "./pages/NotFound";
+import MyPage from "./pages/MyPage";
+import Login from "./pages/Login";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profiles/:username" element={<Profile />} />
+      </Route>
+      <Route path="/articles" element={<Articles />}>
+        <Route path=":id" element={<Article />} />
+      </Route>
+      <Route path = "/login" element = {<Login /> } />
+      <Route path="/mypage" element = {<MyPage />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
+
+export default App;
